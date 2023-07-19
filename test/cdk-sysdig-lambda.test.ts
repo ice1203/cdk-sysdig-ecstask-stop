b2b23b56a903c006af38f15291c67aa717fadd4c
ice1203 <kikuchi3588@gmail.com>
2023-07-19 09:44:54 +0900

    Initial commit

---
+++ test/cdk-sysdig-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSysdigLambda from '../lib/cdk-sysdig-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-sysdig-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSysdigLambda.CdkSysdigLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
